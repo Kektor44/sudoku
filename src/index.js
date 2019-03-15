9866a68bfde639e3da85bc8e4c6c94aef750099b
Vlad Tataritsky <tataritskii@bk.ru>
2019-03-15 19:13:29 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+    for (var horz = 0; horz < 9; horz++) {
+        for (var vert = 0; vert < 9; vert++) {
+            var posVal = matrix[horz][vert];
+            if (posVal === 0) {
+                matrix[horz][vert] = getNum(matrix, horz, vert);
+            }
+        }
+    }
+    return matrix;
+};
+function getNum(matrix, horz, vert) {
+    var horzCand = [];
+    var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    
+    var writtenNum = matrix[horz].reduce(function (set, posVal) {
+        return set.add(posVal);
+    }, new Set());
+
+    var vertSet = new Set();
+    for (var horzNumber = 0; horzNumber < 9; horzNumber++) {
+        vertSet.add(matrix[horzNumber][vert]);
+    }
+    
+    var vertCand = num.filter(function (number) {
+        return !vertSet.has(number);
+    });
+    if (vertCand.size === 1) {
+        return [...vertCand][0];
+    }
+    horzCand = num.filter(function (number) {
+            return !writtenNum.has(number);
+    });
+    if (horzCand.size === 1) {
+        return [...horzCand][0];
+    }
+    return horzCand;
 }
